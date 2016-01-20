9f524565cb5c10321303e7053cd789a174227b08
Brian Mock <brian@mockbrian.com>
2016-01-20 12:44:03 -0800

    Collapses two modules together

--- src/weather-underground.js
+++ src/weather-underground.js
@@ -1,11 +1,16 @@
-var Request = require('./request');
+const Bluebird = require('bluebird');
+const request = Bluebird.promisify(require('request'));
+
+function getJSON(url) {
+  return request(url).then(r => r.toJSON());
+}
 
 function withKey(API_KEY) {
   var PREFIX = 'https://api.wunderground.com/api/' + API_KEY;
 
   function weather(zipCode) {
     const url = PREFIX + '/conditions/q/' + zipCode + '.json';
-    return Request.getJSON(url)
+    return getJSON(url)
       .then(x => x.body)
       .then(JSON.parse);
   }
